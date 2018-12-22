b2e58786706a0b203a7cf2bf170c16c8d5b48f3e
Gabriel Cabrera <gabrielmscabrera@gmail.com>
2018-12-22 05:24:19 -0700

    added files, full functionality

---
+++ main.js
@@ -0,0 +1,21 @@
+//Main.js//
+
+let getBill = document.getElementById(`bill`);
+let getPercent = document.getElementById(`percent`);
+let getTip = document.getElementById(`tip`);
+let getTotal = document.getElementById(`total`);
+let calcBtn = document.getElementById(`calcButton`);
+
+calcBtn.addEventListener(`click`, calc)
+
+function calc(e) {
+	e.preventDefault();
+	var bill = parseFloat(getBill.value);
+	var tip = parseFloat(getPercent.value);
+	var percent = tip / 100;
+	var tipTotal = bill * percent;
+	var billTotal = bill + tipTotal;
+
+	getTip.value = (tipTotal).toFixed(2);
+	getTotal.value = (billTotal).toFixed(2);
+}
